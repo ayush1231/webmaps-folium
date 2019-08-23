9ac238d235a9aecc9ddc841ee9b9db945b2eca1b
Ayush Sharma <ayushsharma@Ayushs-MacBook-Pro.local>
2019-08-22 21:14:35 -0700

    Added comments in script.py

--- script.py
+++ script.py
@@ -4,11 +4,13 @@ import json
 
 data = pandas.read_csv("Volcanoes_USA.txt")
 
+# Creating lists for different parameters in the .txt file
 lat = list(data.LAT)
 lon = list(data.LON)
 elev = list(data.ELEV)
 name = list(data.NAME)
 
+# html string for searching about a given volcano on google.com
 html = """
 Volcano name:<br>
 <a href="https://www.google.com/search?q={}" target="_blank">{}</a><br>
@@ -16,6 +18,7 @@ Height: {} m
 """
 
 
+# Color criteria based on elevation of volcanoes
 def color_mapping(elevation):
 
     if elevation < 1000:
@@ -30,7 +33,7 @@ def color_mapping(elevation):
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=5, tiles='Stamen Terrain')
 
-
+# One feature group per feature - volcanoes and population
 fg_volcano = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
@@ -45,6 +48,11 @@ for lt, ln, el, name in zip(lat, lon, elev, name):
 
 fg_population = folium.FeatureGroup("Population")
 
+"""
+Shows green color for population parts less than 10 million. Orange for areas between 10 to 20 millions.
+For population greater than 20 million, it will show red color
+
+"""
 fg_population.add_child(folium.GeoJson(json.load(open("world.json", "r", encoding="utf-8-sig")),
                             style_function=lambda x: {'fillColor':'green' if x["properties"]["POP2005"] < 10000000
                             else 'orange' if 10000000 <= x['properties']['POP2005'] <= 20000000 else 'red'}))
